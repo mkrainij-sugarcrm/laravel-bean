de3c17e3543d769f8397dc93fa4a60de82fe032d
Mikhail Kozlov <code@315design.com>
2015-06-30 11:20:09 -0700

    hacky update to make it work short term

--- src/Sugarcrm/Bean/Relations/HasMany.php
+++ src/Sugarcrm/Bean/Relations/HasMany.php
@@ -1,5 +1,6 @@
 <?php namespace Sugarcrm\Bean\Relations;
 
+use Illuminate\Database\Eloquent\Collection;
 use Sugarcrm\Bean\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany as EloquentHasMany;
 
@@ -18,7 +19,26 @@ class HasMany extends EloquentHasMany
      */
     public function get(array $columns = [])
     {
-        return $this->query->setupParent($this->getParent()->getModule(), $this->getForeignKey())->get($columns);
-    }
+        $results = $this->query->getQuery()->getConnection()->related(
+            $this->getParent()->getTable(),
+            $this->getParent()->id,
+            $this->getForeignKey(),
+            ['fields' => $columns]
+        );
 
+        if(!array_key_exists('records',$results)){
+
+            $this->initRelation([$this->getParent()],$this->getForeignKey());
+
+            return $this->getParent()->getRelation($this->getForeignKey());
+        }
+
+        foreach ($results['records'] as $i => $result) {
+            $results['records'][$i] = $this->related->newInstance($result);
+        }
+
+        $this->getParent()->setRelation($this->getForeignKey(), $this->related->newCollection($results['records']));
+
+        return $this->getParent()->getRelation($this->getForeignKey());
+    }
 }
